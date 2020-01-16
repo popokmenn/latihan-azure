003b2c4bf5521bd26e7847d39bd10054da2c70fa
Naufal Retyan Ajisasongko <42527596+popokmenn@users.noreply.github.com>
2020-01-16 13:56:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -101,55 +101,6 @@
         } catch(Exception $e) {
             echo "Failed: " . $e;
         }
-    } else if (isset($_POST['analyze_img'])) {
-            echo '<script type="text/javascript">
-            function processImage() {
-                var subscriptionKey = "cd4bf6bbc03f4551ac4454522f1d5146";
-                var uriBase ="https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
-       
-                // Request parameters.
-                var params = {
-                    "visualFeatures": "Categories,Description,Color",
-                    "details": "",
-                    "language": "en",
-                };
-       
-                // Display the image.
-                var sourceImageUrl = document.getElementById("inputImage").value;
-                document.querySelector("#sourceImage").src = sourceImageUrl;
-       
-                // Make the REST API call.
-                $.ajax({
-                    url: uriBase + "?" + $.param(params),
-       
-                    // Request headers.
-                    beforeSend: function(xhrObj){
-                        xhrObj.setRequestHeader("Content-Type","application/json");
-                        xhrObj.setRequestHeader(
-                            "Ocp-Apim-Subscription-Key", subscriptionKey);
-                    },
-       
-                    type: "POST",
-       
-                    // Request body.
-                    data: '{"url": ' + '"' + sourceImageUrl + '"}',
-                })
-       
-                .done(function(data) {
-                    // Show formatted JSON on webpage.
-                    $("#responseTextArea").val(JSON.stringify(data, null, 2));
-                })
-       
-                .fail(function(jqXHR, textStatus, errorThrown) {
-                    // Display error message.
-                    var errorString = (errorThrown === "") ? "Error. " :
-                        errorThrown + " (" + jqXHR.status + "): ";
-                    errorString += (jqXHR.responseText === "") ? "" :
-                        jQuery.parseJSON(jqXHR.responseText).message;
-                    alert(errorString);
-                });
-            };
-        </script>';
     }
  ?>
 
