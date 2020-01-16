cc8c7acf695369d0cb363a7066144c302eb35616
Naufal Retyan Ajisasongko <42527596+popokmenn@users.noreply.github.com>
2020-01-16 13:52:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -23,9 +23,8 @@
        Name  <input type="text" name="name" id="name"/></br></br>
        Email <input type="text" name="email" id="email"/></br></br>
        Job <input type="text" name="job" id="job"/></br></br>
-       <input type="text" name="inputImage" id="inputImage" 
-              value="http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg" />
-       <button onclick="processImage()">Analyze image</button>
+       <input type="text" name="inputImage" id="inputImage" value="http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg" />
+       <input type="submit" name="analyze_img" value="Analyze Image" />
        <input type="submit" name="submit" value="Submit" />
        <input type="submit" name="load_data" value="Load Data" />
  </form>
@@ -102,70 +101,55 @@
         } catch(Exception $e) {
             echo "Failed: " . $e;
         }
-    } else if (isset($_POST['analyze_image'])) {
-           echo "<script type=\"text/javascript\">
+    } else if (isset($_POST['analyze_img'])) {
+            echo '<script type="text/javascript">
             function processImage() {
-         // **********************************************
-         // *** Update or verify the following values. ***
-         // **********************************************
-
-         // Replace <Subscription Key> with your valid subscription key.
-         var subscriptionKey = "cd4bf6bbc03f4551ac4454522f1d5146";
-
-         // You must use the same Azure region in your REST API method as you used to
-         // get your subscription keys. For example, if you got your subscription keys
-         // from the West US region, replace "westcentralus" in the URL
-         // below with "westus".
-         //
-         // Free trial subscription keys are generated in the "westus" region.
-         // If you use a free trial subscription key, you shouldn't need to change
-         // this region.
-         var uriBase =
-             "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
-
-         // Request parameters.
-         var params = {
-             "visualFeatures": "Categories,Description,Color",
-             "details": "",
-             "language": "en",
-         };
-
-         // Display the image.
-         var sourceImageUrl = document.getElementById("inputImage").value;
-         document.querySelector("#sourceImage").src = sourceImageUrl;
-
-         // Make the REST API call.
-         $.ajax({
-             url: uriBase + "?" + $.param(params),
-
-             // Request headers.
-             beforeSend: function(xhrObj){
-                 xhrObj.setRequestHeader("Content-Type","application/json");
-                 xhrObj.setRequestHeader(
-                     "Ocp-Apim-Subscription-Key", subscriptionKey);
-             },
-
-             type: "POST",
-
-             // Request body.
-             data: '{"url": ' + '"' + sourceImageUrl + '"}',
-         })
-
-         .done(function(data) {
-             // Show formatted JSON on webpage.
-             $("#responseTextArea").val(JSON.stringify(data, null, 2));
-         })
-
-         .fail(function(jqXHR, textStatus, errorThrown) {
-             // Display error message.
-             var errorString = (errorThrown === "") ? "Error. " :
-                 errorThrown + " (" + jqXHR.status + "): ";
-             errorString += (jqXHR.responseText === "") ? "" :
-                 jQuery.parseJSON(jqXHR.responseText).message;
-             alert(errorString);
-         });
-     };
-            </script>";
+                var subscriptionKey = "cd4bf6bbc03f4551ac4454522f1d5146";
+                var uriBase ="https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+       
+                // Request parameters.
+                var params = {
+                    "visualFeatures": "Categories,Description,Color",
+                    "details": "",
+                    "language": "en",
+                };
+       
+                // Display the image.
+                var sourceImageUrl = document.getElementById("inputImage").value;
+                document.querySelector("#sourceImage").src = sourceImageUrl;
+       
+                // Make the REST API call.
+                $.ajax({
+                    url: uriBase + "?" + $.param(params),
+       
+                    // Request headers.
+                    beforeSend: function(xhrObj){
+                        xhrObj.setRequestHeader("Content-Type","application/json");
+                        xhrObj.setRequestHeader(
+                            "Ocp-Apim-Subscription-Key", subscriptionKey);
+                    },
+       
+                    type: "POST",
+       
+                    // Request body.
+                    data: '{"url": ' + '"' + sourceImageUrl + '"}',
+                })
+       
+                .done(function(data) {
+                    // Show formatted JSON on webpage.
+                    $("#responseTextArea").val(JSON.stringify(data, null, 2));
+                })
+       
+                .fail(function(jqXHR, textStatus, errorThrown) {
+                    // Display error message.
+                    var errorString = (errorThrown === "") ? "Error. " :
+                        errorThrown + " (" + jqXHR.status + "): ";
+                    errorString += (jqXHR.responseText === "") ? "" :
+                        jQuery.parseJSON(jqXHR.responseText).message;
+                    alert(errorString);
+                });
+            };
+        </script>';
     }
  ?>
 
@@ -173,23 +157,8 @@
 
  <script type="text/javascript">
      function processImage() {
-         // **********************************************
-         // *** Update or verify the following values. ***
-         // **********************************************
-
-         // Replace <Subscription Key> with your valid subscription key.
          var subscriptionKey = "cd4bf6bbc03f4551ac4454522f1d5146";
-
-         // You must use the same Azure region in your REST API method as you used to
-         // get your subscription keys. For example, if you got your subscription keys
-         // from the West US region, replace "westcentralus" in the URL
-         // below with "westus".
-         //
-         // Free trial subscription keys are generated in the "westus" region.
-         // If you use a free trial subscription key, you shouldn't need to change
-         // this region.
-         var uriBase =
-             "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+         var uriBase ="https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
 
          // Request parameters.
          var params = {
