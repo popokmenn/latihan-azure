5e27c72ed38b689c3ff4aa0ed4acbadb50faf40c
Naufal Retyan Ajisasongko <42527596+popokmenn@users.noreply.github.com>
2020-01-16 14:15:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -110,7 +110,7 @@
      console.log("called");
      function processImage() {
          var subscriptionKey = "cd4bf6bbc03f4551ac4454522f1d5146";
-         var uriBase ="https://naufal-computer-vision.cognitiveservices.azure.com/";
+         var uriBase ="https://southeastasia.api.cognitive.microsoft.com/";
 
          // Request parameters.
          var params = {
