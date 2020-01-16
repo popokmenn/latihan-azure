e8efe8e52b8034b02136d7646c4dfde7be357519
Naufal Retyan Ajisasongko <42527596+popokmenn@users.noreply.github.com>
2020-01-16 13:24:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
  <head>
  <Title>Registration Form</Title>
+ <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
  <style type="text/css">
  	body { background-color: #fff; border-top: solid 10px #000;
  	    color: #333; font-size: .85em; margin: 20; padding: 20;
@@ -14,7 +15,6 @@
  	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
  	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
  </style>
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
  </head>
  <body>
  <h1>Register here!</h1>
